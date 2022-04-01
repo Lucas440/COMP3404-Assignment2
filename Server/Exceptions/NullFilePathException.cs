2c3f94e1aee7492d7f1b49d39d54b8a1dbb2eb4b
FlynnOsborne <96054775+FlynnOsborne@users.noreply.github.com>
2022-04-01 16:10:13 +0100

    New Exception

---
+++ Server/Exceptions/NullFilePathException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// AUTHOR: Flynn Osborne
+/// DATE: 01/04/2022
+/// </summary>
+
+namespace Server.Exceptions
+{
+    /// <summary>
+    /// An exception that is thrown when the file path returned is null
+    /// </summary>
+    public class NullFilePathException : Exception
+    {
+        /// <summary>
+        /// The default constructor
+        /// </summary>
+        public NullFilePathException() : base() { }
+
+        /// <summary>
+        /// The recommended constructor
+        /// </summary>
+        /// <param name="message">The message shown to whatever caused the exception</param>
+        public NullFilePathException(string message) : base(message) { }
+
+        /// <summary>
+        /// A constructor for when other exceptions are queued
+        /// </summary>
+        /// <param name="message">The message shown to whatever caused the exception</param>
+        /// <param name="e">The queued exception</param>
+        public NullFilePathException(string message, Exception e) : base(message, e) { }
+    }
+}
