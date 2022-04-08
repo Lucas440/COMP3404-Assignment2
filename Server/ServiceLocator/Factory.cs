8d7c0ee7aaaec4c27d447f3d4f03ef3b058de9e7
FlynnOsborne <96054775+FlynnOsborne@users.noreply.github.com>
2022-04-08 15:16:19 +0100

    Comments

--- Server/ServiceLocator/Factory.cs
+++ Server/ServiceLocator/Factory.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 /// <summary>
-/// Author Lucas Brennan
-/// 
-/// Date: 04/03/22
+/// AUTHOR: Lucas Brennan
+/// DATE: 04/03/22
 /// </summary>
 namespace Server.ServiceLocator
 {
@@ -16,14 +16,14 @@ namespace Server.ServiceLocator
     public class Factory : IFactory
     {
         /// <summary>
-        /// A Class that returns a new instence of class type C
+        /// A method that returns a new instance of class type C
         /// </summary>
-        /// <typeparam name="T">A Type defined when the class is intitalised</typeparam>
+        /// <typeparam name="T">A type defined when the class is intitalised</typeparam>
         /// <typeparam name="C">A child class of class T</typeparam>
-        /// <returns>A new intence of T</returns>
+        /// <returns>A new instance of T</returns>
         public T Get<T , C>() where C: T, new() 
         {
-            //Retuns a new Class C
+            // RETURN a new instance of the class
             return new C();
         }
     }
--- Server/ServiceLocator/ServiceLocator.cs
+++ Server/ServiceLocator/ServiceLocator.cs
@@ -3,47 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 /// <summary>
-/// Author Lucas Brennan & Flynn Osborne
-/// 
-/// Date 03/04/22
+/// AUTHOR: Lucas Brennan & Flynn Osborne
+/// DATE: 03/04/22
 /// </summary>
 namespace Server.ServiceLocator
 {
     /// <summary>
-    /// A Class used to create and store serivces 
+    /// A class used to create and store services 
     /// </summary>
     public class ServiceLocator : IServiceLocator
     {
-        //DECLARE a new IDictionary called _serviceCollection
+        // DECLARE a new IDictionary called _serviceCollection
         IDictionary<Type, IService> _serviceCollection;
-        //DECLARE a new IFactory called _serviceFactory
+
+        // DECLARE a new IFactory called _serviceFactory
         IFactory _serviceFactory;
+
         /// <summary>
         /// The Default Constructor for ServiceLocator
         /// </summary>
         public ServiceLocator()
         {
-            //INTALISE Class Variables
+            // INITIALISE Class Variables
             //_serviceCollection
             _serviceCollection = new Dictionary<Type, IService>();
             //_serviceFactory
             _serviceFactory = new Factory();
         }
         /// <summary>
-        /// A Mehtod that retruns a requested service of type T
+        /// A method that returns a requested service of type T
         /// </summary>
-        /// <typeparam name="T">The Product of the service</typeparam>
-        /// <returns>The Service that creates the Product</returns>
+        /// <typeparam name="T">The product of the service</typeparam>
+        /// <returns>The service that creates the product</returns>
         public IService Get<T, C>()
         {
-            //If _serviceCollection does not contain a key of type T
+            // IF _serviceCollection does not contain a key of type T
             if (!_serviceCollection.ContainsKey(typeof(T)))
             {
-                //Adds a new Factory that creates T to _serviceCollection
+                // ADD a new Factory that creates T to _serviceCollection
                 _serviceCollection.Add(typeof(T), _serviceFactory.Get<IFactory , Factory>());
             }
-            //Returns the Service stored with the key type T
+
+            // RETURN the service stored with the key type T
             return _serviceCollection[typeof(T)];
         }
     }
